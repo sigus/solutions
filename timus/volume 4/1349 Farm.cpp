2296503db734b17605f297d8f46def531d75f29b
megachuhancer <megachuhancer@gmail.com>
2011-05-02 21:20:58 +1100

    add solution for 1349

---
+++ timus/volume 4/1349 Farm.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main() {
+    int n;
+    scanf(" %d", &n);
+    if (n == 1) 
+        puts("1 2 3");
+    else if (n == 2) 
+        puts("3 4 5");
+    else
+        puts("-1");
+    return 0;
+}
