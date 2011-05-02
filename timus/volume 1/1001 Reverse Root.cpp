6e7b68e4036b0629637ce6ef5669ab237679508b
megachuhancer <megachuhancer@gmail.com>
2011-05-02 21:03:25 +1100

    rewritten with c++ vector, long double -> double

--- timus/volume 1/1001 Reverse Root.cpp
+++ timus/volume 1/1001 Reverse Root.cpp
@@ -1,14 +1,14 @@
 #include <cstdio>
 #include <cmath>
-
-long double d[250000];
+#include <vector>
+using namespace std;
 
 int main() {
-    int p = 0;
-    long double ld = 0.0;
-    while (scanf(" %lf", &ld) != EOF) 
-        d[p++] = sqrt(ld);
-    for (int i = p - 1; i >= 0; i--) 
-        printf("%.4lf\n", d[i]);
+    vector<double> v;
+    double d = 0.0;
+    while (scanf(" %lf", &d) != EOF) 
+        v.push_back(sqrt(d));
+    for (int i = v.size() - 1; i >= 0; i--) 
+        printf("%lf\n", v[i]);
     return 0;
 }
