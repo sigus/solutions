bebe0071bcf96f464cbfdc9bb931ed7772dfd080
megachuhancer <megachuhancer@gmail.com>
2011-06-21 01:16:37 +1100

    solution without ceil, add newline at end of file

--- cf/Codeforces Beta Round #74/div 2/A. Cableway.cpp
+++ cf/Codeforces Beta Round #74/div 2/A. Cableway.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <algorithm>
-#include <cmath>
 using namespace std;
 
 int main() {
     int r, g, b;
     cin >> r >> g >> b;
-    cout << max(27 + 3*((int) ceil(r/2.0)), max(28 + 3*((int) ceil(g/2.0)), 29 + 3*((int) ceil(b/2.0))));
+    cout << max(27 + 3*((r + 1)/2), max(28 + 3*((g + 1)/2), 29 + 3*((b + 1)/2)));
     return 0;
 }
