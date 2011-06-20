05502b0858efbcd13a9fb511971a5cb22f0caa0b
megachuhancer <megachuhancer@gmail.com>
2011-06-21 01:40:25 +1100

    shorter solution

--- cf/Codeforces Beta Round #74/div 2/B. African Crossword.cpp
+++ cf/Codeforces Beta Round #74/div 2/B. African Crossword.cpp
@@ -13,22 +13,17 @@ int main() {
         v.push_back(str);
     }
 
-    vector< vector<bool> > striked(n, vector<bool>(m));
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++) {
-            for (int k = i + 1; k < n; k++)
-                if (v[k][j] == v[i][j])
-                    striked[k][j] = striked[i][j] = true;
-            for (int k = j + 1; k < m; k++)
-                if (v[i][k] == v[i][j])
-                    striked[i][k] = striked[i][j] = true;
+            bool flg = true;
+            for (int k = 0; k < n; k++)
+                if (v[k][j] == v[i][j] && k != i)
+                    flg = false;
+            for (int k = 0; k < m; k++)
+                if (v[i][k] == v[i][j] && k != j)
+                    flg = false;
+            if (flg)
+                cout << v[i][j];
         }
-    string ans;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            if (!striked[i][j])
-                ans += v[i][j];
-
-    cout << ans;
     return 0;
 }
