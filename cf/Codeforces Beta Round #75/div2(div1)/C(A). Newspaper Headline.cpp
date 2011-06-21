c4c8ede9356c9a89a72050285f96f24e1d8956fa
megachuhancer <megachuhancer@gmail.com>
2011-06-21 20:05:10 +1100

    O(s1*K + s2)

--- cf/Codeforces Beta Round #75/div2(div1)/C(A). Newspaper Headline.cpp
+++ cf/Codeforces Beta Round #75/div2(div1)/C(A). Newspaper Headline.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <string> 
-#include <algorithm>
 using namespace std;
 
 int main() {
     string s1, s2;
     cin >> s1 >> s2;
 
-    vector<int> l[256];
-    for (int i = 0; i < s1.length(); i++)
-        l[s1[i]].push_back(i);
+    vector< vector<int> > l(s1.length() + 1, vector<int>(256, s1.length()));
+    for (int i = s1.length() - 1; i >= 0; i--)
+        for (char j = 'a'; j <= 'z'; j++)
+            if (s1[i] == j)
+                l[i][j] = i;
+            else if (i + 1 < s1.length())
+                l[i][j] = l[i + 1][j];
+                
     int ans = 1, pos = 0;
     for (int i = 0; i < s2.length(); i++) {
-        vector<int>::iterator j = lower_bound(l[s2[i]].begin(), l[s2[i]].end(), pos);
-        if (j == l[s2[i]].end()) {
-            pos = 0;
+        pos = l[pos][s2[i]];
+        if (pos == s1.length()) {
             ans++;
-            j = lower_bound(l[s2[i]].begin(), l[s2[i]].end(), pos);
-            if (j == l[s2[i]].end()) {
+            pos = 0;
+            pos = l[pos][s2[i]];
+            if (pos == s1.length()) {
                 cout << -1;
                 return 0;
             }
         }
-        pos = *j + 1;
+        pos++;
     }
 
     cout << ans;
