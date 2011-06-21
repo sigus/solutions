2e4e77d596eee585be0dff37c4fd6016ea211bcc
megachuhancer <megachuhancer@gmail.com>
2011-06-21 15:07:45 +1100

    solution without list

--- cf/Codeforces Beta Round #75/div 2/B. Binary Number.cpp
+++ cf/Codeforces Beta Round #75/div 2/B. Binary Number.cpp
@@ -1,36 +1,28 @@
-#include <iostream>
-#include <string> 
-#include <list>
-using namespace std;
+#include <cstdio>
+#include <cstring>
 
 int main() {
-    string x;
-    cin >> x;
-    list<int> l;
-    for (int i = 0; i < x.size(); i++)
-        l.push_back(x[i] - '0');
+    char x[1000001];
+    gets(x);
+    int l = strlen(x);
+    for (int i = 0; i < l; i++)
+        x[i] -= '0';
 
     int ans = 0;
-    while (l.size() != 1 || l.front() != 1) {
+    for (int i = l - 1; i > 0; i--) {
+        bool c = false;
+        for (; i >= 0 && x[i]; i--) {
+            ans++;
+            c = true;
+        }
+        if (i >= 0) {
+            x[i] = 1;
+            if (c)
+                i++;
+        }
         ans++;
-        if (!l.back())
-            l.pop_back();
-        else {
-            list<int>::iterator i = l.end();
-            i--;
-            while (*i == 1) {
-                *i = 0;
-                if (i == l.begin())
-                    break;
-                i--;
-            }
-            if (i == l.begin())
-                l.push_front(1);
-            else
-                *i = 1;
-        }    
     }
 
-    cout << ans;
+    printf("%d", ans);
     return 0;
 }
