8776fdf9e65bda65b5361396a4634e8ccfa754f7
megachuhancer <megachuhancer@gmail.com>
2011-06-28 16:19:31 +1100

    add solution for tc srm510 div2 250-point problem

---
+++ tc/SRMs/SRM 510/div 2/TheAlmostLuckyNumbersDivTwo(250).cpp
@@ -0,0 +1,19 @@
+class TheAlmostLuckyNumbersDivTwo { 
+    int f(int a) { 
+        int cnt = 0; 
+        while(a) { 
+            if (a%10 != 4 && a%10 != 7) 
+              cnt++; 
+            a /= 10; 
+        } 
+        return cnt <= 1;         
+    } 
+public: 
+    int find(int a, int b) { 
+      	int ans = 0; 
+        for (int i = a; i <= b; i++) 
+            if (f(i)) 
+                ans++; 
+        return ans; 
+    } 
+};
