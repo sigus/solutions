cc43c872b96cdb1d6e846c1a0c29ab4e5730e42e
megachuhancer <megachuhancer@gmail.com>
2011-07-02 18:44:01 +1100

    add solution for tc srm507 div2 250-point problem

---
+++ tc/SRMs/SRM 507/div 2/CubeAnts(250).cpp
@@ -0,0 +1,14 @@
+#include <vector>
+using namespace std;
+
+class CubeAnts {
+public:
+    int getMinimumSteps(vector <int> pos) {
+        int m[8] = { 0, 1, 2, 1, 1, 2, 3, 2 };
+        int mi = 0;
+        for (int i = 0; i < pos.size(); i++)
+            if (m[pos[i]] > m[mi])
+                mi = pos[i];
+        return m[mi];
+	}
+};
