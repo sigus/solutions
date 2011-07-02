6eaaf13d7fdc2167f5a9cbd0e3cbfb5e35402625
megachuhancer <megachuhancer@gmail.com>
2011-07-02 19:36:28 +1100

    add solution for tc srm504.5 div2 250-point problem

---
+++ tc/SRMs/SRM 504.5/div 2/TheJackpotDivTwo(250).cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class TheJackpotDivTwo {
+public:
+    vector <int> find(vector <int> money, int jackpot) {
+        while (jackpot--) {
+            int m = 0;
+            for (int i = 1; i < money.size(); i++)
+                if (money[i] < money[m])
+                    m = i;
+            money[m]++;
+        }
+        sort(money.begin(), money.end());
+        return money;
+    }
+};
